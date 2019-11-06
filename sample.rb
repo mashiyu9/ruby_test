608a2e40cdc63332fe57df94ba6f60c82d0d3eac
yu shimamura <mashiyu0914@gmail.com>
2019-11-06 17:19:34 +0900

    fix: バグの修正
    アルファベットを入力してしまった時の処理

--- sample.rb
+++ sample.rb
@@ -1,12 +1,13 @@
 class Player
   def hand
     while true 
-      puts "出す手を選んでください 0 : グー, 1 : チョキ, 2 : パー"
-      my_hand = gets.to_i
-      if my_hand <=2 && my_hand >= 0
-        return my_hand
+      puts "出す手を選んでください 0 : グー 1 : チョキ, 2 : パー"
+      my_hand = gets.chomp
+      p my_hand
+      if (my_hand == "0") || (my_hand == "1") || (my_hand == "2")
+        return my_hand.to_i
       else
-        puts "0~2の数字を選択してください" 
+        puts "0~2の数字を入力してください" 
       end
     end
     # コンソールを入力待ち状態にし、プレイヤーがコンソールから打ち込んだ値を出力する処理のメソッドの処理をこの中に作成する
@@ -15,7 +16,7 @@ end
 
 class Enemy
   def hand
-    enemy_hand = rand(2)
+    enemy_hand = rand(2).to_i
     # グー、チョキ、パーの値をランダムに出力するメソッドの処理をこの中に作成する  
   end
 end
