c53cfef589f41eea5acbc01ab829369fa01b06b9
yu shimamura <mashiyu0914@gmail.com>
2019-11-06 16:07:05 +0900

    add: update じゃんけんプログラム
    ヒントを見ての改良

--- sample.rb
+++ sample.rb
@@ -9,63 +9,38 @@ class Player
         puts "0~2の数字を選択してください" 
       end
     end
+    # コンソールを入力待ち状態にし、プレイヤーがコンソールから打ち込んだ値を出力する処理のメソッドの処理をこの中に作成する
   end
 end
 
 class Enemy
   def hand
-    enemy_hand = rand(3) + 1
+    enemy_hand = rand(2)
     # グー、チョキ、パーの値をランダムに出力するメソッドの処理をこの中に作成する  
   end
 end
 
 class Janken
   def pon(player_hand, enemy_hand)
-    if player_hand == 0 
-      if enemy_hand == 1
-        puts "相手の手はグー"
-        puts "引き分けです"
-      elsif enemy_hand == 2
-        puts "相手の手はチョキ}"
-        puts "あなたの勝ちです"
-      elsif enemy_hand == 3
-        puts "相手の手はパーです"
-        puts "あなたの負けです"
-      end
-    elsif     
-      if player_hand == 1 
-        if enemy_hand == 1
-          puts "相手の手はグー"
-          puts "あなたの負けです"
-        elsif enemy_hand == 2
-          puts "相手の手はチョキ"
-          puts "引き分けです"
-        elsif enemy_hand == 3
-          puts "相手の手はパー"
-          puts "あなたの勝ちです"
-        end
-      end
-    elsif player_hand == 2
-      if enemy_hand == 1
-        puts "相手の手はグー"
-        puts "あなたの勝ちです"
-      elsif enemy_hand == 2
-        puts "相手の手はチョキ"
-        puts "あなたの負けです"
-      elsif enemy_hand == 3
-        puts "相手の手はパーです"
-        puts "引き分けです"
-      end
+    # janken = Janken.new
+    janken_juge = (player_hand - enemy_hand + 3)
+    if janken_juge % 3 == 0 
+      puts "相手は#{enemy_hand}を出しました"
+      puts "引き分けです"
+      player = Player.new
+      enemy = Enemy.new
+      janken = Janken.new
+      janken.pon(player.hand, enemy.hand)
+      # janken.pon(player.hand,enemy.hand)
+    elsif janken_juge % 3 == 1
+      puts "相手は#{enemy_hand}を出しました"
+      puts "あなたの負けです"
+      return 
+    elsif janken_juge % 3 == 2
+      puts "相手は#{enemy_hand}を出しました"
+      puts "あなたの勝ちです"
+      return 
     end
-
-
-      
-
-      
-
-
-    # その際、あいこもしくはグー、チョキ、パー以外の値入力時には、もう一度ジャンケンをする
-    # 相手がグー、チョキ、パーのうち、何を出したのかも表示させる
   end
 end
 
@@ -74,6 +49,6 @@ enemy = Enemy.new
 janken = Janken.new
 
 # 下記の記述で、ジャンケンメソッドが起動される
-janken.pon(player.hand, enemy.hand)
-
-# ここに書かれているRubyの記述はあくまでヒントとして用意された雛形なので
+# while player.hand == enemy.hand
+  janken.pon(player.hand, enemy.hand)
+# end
